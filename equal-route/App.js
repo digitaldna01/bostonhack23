29fdb240fe6d11fb566c0fe38097b1e38c0bd3cc
Alice Han <alicehan@crc-dot1x-nat-10-239-1-181.bu.edu>
2023-11-18 20:07:59 -0500

    trying to initialize at current location

--- equal-route/App.js
+++ equal-route/App.js
@@ -3,19 +3,41 @@ import { StyleSheet, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
 import { PROVIDER_GOOGLE } from 'react-native-maps';
 
+
+// Current Location
+navigator.geolocation.getCurrentPosition(
+  (position) => {
+    const pos = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude,
+    };
+  }
+)
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is a test!</Text>
+      {/* <Text>This is a test!</Text> */}
       <MapView
-        style={{ height: '50%', width: '100%' }}
+        style={{ height: '110%', width: '100%' }}
         provider={PROVIDER_GOOGLE}
         showsUserLocation={true}
+        initialRegion={{
+          latitude: currentLocation.latitude,
+          // position.coords.latitude,
+          longitude: currentLocation.longitude,
+          // position.coords.longitude,
+          //latitudeDelta: .1,
+          //longitudeDelta: .1,
+        }}
       />
       <StatusBar style="auto" />
     </View>
   );
 }
+
+
+
 
 const styles = StyleSheet.create({
   container: {
