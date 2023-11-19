d6879097d35140f768c8b653166ffd9c7b4a525e
Alice Han <alicehan@crc-dot1x-nat-10-239-1-181.bu.edu>
2023-11-18 19:28:27 -0500

    google maps api

--- equal-route/App.js
+++ equal-route/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
+import { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default function App() {
   return (
@@ -8,6 +9,8 @@ export default function App() {
       <Text>This is a test!</Text>
       <MapView
         style={{ height: '50%', width: '100%' }}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation={true}
       />
       <StatusBar style="auto" />
     </View>
