7c093d34513ff26b7e6007c2e24fb79ccea45562
Alice Han <alicehan@crc-dot1x-nat-10-239-1-181.bu.edu>
2023-11-18 21:34:27 -0500

    working on stuff

--- equal-route/App.js
+++ equal-route/App.js
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+import React, { useState } from 'react';
 
+let map, infoWindow;
 
-// Current Location
-navigator.geolocation.getCurrentPosition(
-  (position) => {
-    const pos = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude,
-    };
-  }
-)
 
 export default function App() {
+  //const pos = { lat: 0, lng: 0 };
+
+  //Current Location
+  if (navigator.geolocation) {
+    console.log("working here");
+    navigator.geolocation.getCurrentPosition(successCallback)((position) => {
+      const pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    console.log("going to else");
+    pos = {
+      lat: 42.35062656407261,
+      lng: -71.10539373555561,
+    }
+  }
+
+  // Data Science Building
+  const [destination, setDestination] = useState({
+    latitude: 42.35009532692169,
+    longitude: -71.10325692205237
+  });
+
+  const [origin, setOrigin] = useState({
+    latitude: pos.lat,
+    longitude: pos.lng
+  });
+
   return (
     <View style={styles.container}>
       {/* <Text>This is a test!</Text> */}
@@ -23,14 +48,33 @@ export default function App() {
         provider={PROVIDER_GOOGLE}
         showsUserLocation={true}
         initialRegion={{
-          latitude: currentLocation.latitude,
+          latitude: pos.lat,
+          //currentLocation.latitude,
           // position.coords.latitude,
-          longitude: currentLocation.longitude,
+          longitude: pos.lng,
+          //currentLocation.longitude,
           // position.coords.longitude,
           //latitudeDelta: .1,
           //longitudeDelta: .1,
         }}
-      />
+      >
+        <MapViewDirections
+          origin={origin}
+          destination={destination}
+          apikey="AIzaSyDXdNULA8JJynfh30jdKn0uPRvvVyIPe1U"
+          strokeWidth={4}
+          strokeColor="red"
+          mode={'WALKING'}
+        />
+        <Marker
+          coordinate={origin}
+          title="Starting Point"
+        />
+        <Marker
+          coordinate={destination}
+          title="Destination Point"
+        />
+      </MapView>
       <StatusBar style="auto" />
     </View>
   );
@@ -47,3 +91,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your App doesn't support geolocation.",
+  );
+  infoWindow.open(map);
+}
+
+
