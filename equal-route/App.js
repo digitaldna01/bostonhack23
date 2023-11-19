fbe3979912e14aa5c715f44198c9117977828c9c
Alice Han <alicehan@crc-dot1x-nat-10-239-1-181.bu.edu>
2023-11-18 19:13:23 -0500

    added map

--- equal-route/App.js
+++ equal-route/App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is a test!</Text>
+      <MapView
+        style={{ height: '50%', width: '100%' }}
+      />
       <StatusBar style="auto" />
     </View>
   );
